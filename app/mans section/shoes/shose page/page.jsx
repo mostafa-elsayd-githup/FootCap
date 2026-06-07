6d62070edf929db6e8d96b1154b36cbf2ab15a29
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-07 19:59:37 +0300

    refactore: restructuring project files

---
+++ app/mans section/shoes/shose page/page.jsx
@@ -0,0 +1,65 @@
+"use server";
+import Link from "next/link";
+import styles from "./page.module.css";
+import SingleProduct from "./singelproduct";
+import NavAction from "@/Components/Navbar/NavAction";
+import MiniDrowp from "@/Components/minidrowp/minidrowp";
+import { createClientForServer } from "@/utils/supabase";
+
+async function gitdata({ categoryKey }) {
+  try {
+    const createClient = await createClientForServer();
+    const { data, error } = await createClient
+      .from("products")
+      .select("*")
+      .eq("type", categoryKey);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function Product({ searchParams }) {
+  const queryParams = await searchParams;
+  const categoryKey = queryParams.type;
+
+  const data = await gitdata({ categoryKey });
+  return (
+    <>
+      <NavAction />
+      <div className={styles.Container}>
+        <div className={styles.text}>
+          <span className={styles.spans}>
+            <Link
+              className={styles.span}
+              href="/Components/sport-Componente/sportProcuts/sport_from_gemProducts?club=tshirt"
+            >
+              Sport /
+            </Link>
+            <span>
+              {"   "}
+              <Link className={styles.span} href="">
+                Gym & Training
+              </Link>
+            </span>
+          </span>
+          <h1 className={styles.title}>
+            Adidaes Running Collection{" "}
+            <span style={{ fontSize: "15px", color: "#7777" }}>
+              [ {data.length} ]
+            </span>
+          </h1>
+        </div>
+        <MiniDrowp />
+        <div className={styles.products}>
+          {data &&
+            data.map((item) => {
+              return <SingleProduct key={item.id} productItem={item} />;
+            })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Product;
