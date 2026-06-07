18c71fef86daaa8353b7cb64495032d34fe7b3b2
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-07 18:32:06 +0300

    refactor: refactor the code

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 
-import Hreo from "@/app/Hero/hero";
-import Sport from "@/app/sport-Componente/Sport";
-import MenCollection from "@/app/Collection/Collection";
+import Hreo from "@/Components/Hero_com/hero";
+import Sport from "@/Components/sport-Componente/Sport";
+import MenCollection from "@/Components/Collection_com/Collection";
 import Footer from "@/Components/footer/Footre";
-import SportComponete from "@/app/your_sport_start_here_componente/you_sport_componete";
+import SportComponete from "@/Components/your_sport_start_here_componente/you_sport_componete";
 import DiscoundComponent from "@/Components/discound_componente/discounds";
-import HotCOMPONANTE from "@/app/what_is_hot_componante/what_is_hot";
+import HotCOMPONANTE from "@/Components/what_is_hot_componante/what_is_hot";
 import NavAction from "@/Components/Navbar/NavAction";
 import { supabase } from "@/utils/supabase";
 // import "aos/dist/aos.css";
