57ffa5990915827b11692c6acc4249dccd02db3b
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-09 16:27:18 +0300

    use supabase instead of json

--- server/cardpage_server.jsx
+++ server/cardpage_server.jsx
@@ -1,35 +1,46 @@
 "use server";
-import { revalidatePath, revalidateTag } from "next/cache";
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
+import { createClientForServer } from "@/utils/supabase";
+import { revalidatePath } from "next/cache";
+
 const DeleteCart = async (prevstate, formData) => {
   const actionTypeState = formData.get("intent");
   const id = formData.get("id");
-
-  const cookieStors = await cookies();
-  const token = cookieStors.get("token")?.value;
-  const decryption = jwt.verify(token, process.env.JWT_SECRET);
+  const supabaseServer = await createClientForServer();
+  const {
+    data: { user },
+    error: userError,
+  } = await supabaseServer.auth.getUser();
+  if (!user || userError) {
+    return { state: 401 };
+  }
   if (actionTypeState === "delete") {
-    const res = await fetch(`http://localhost:1200/users/${decryption.id}`);
+    const { data: profile, error: userError } = await supabaseServer
+      .from("profiles")
+      .select("cart")
+      .eq("id", user.id)
+      .single();
 
-    if (res) {
-      const userdata = await res.json();
+    let cart = profile.cart || [];
 
-      let cart = userdata.cart || [];
+    const exiest = cart.some((item) => item.id === id);
 
-      const exiest = cart.some((item) => item.id === id);
-
-      if (exiest) {
-        cart = cart.filter((item) => item.id !== id);
-      }
-      await fetch(`http://localhost:1200/users/${decryption.id}`, {
-        method: "PATCH",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ cart: cart }),
-      });
-      
+    if (exiest) {
+      cart = cart.filter((item) => item.id !== id);
     }
-    revalidateTag("navbar");
+    const { error: UserError } = await supabaseServer
+      .from("profiles")
+      .update({ cart: cart })
+      .eq("id", user.id);
+
+    if (UserError) {
+      console.error(
+        "Error updating wishlist in Supabase:",
+        updateError.message,
+      );
+      return { error: "Failed to update wishlist" };
+    }
+    revalidatePath("/");
+    return { cardstate: exiest, status: 200, time: Date.now() };
   }
 };
 export default DeleteCart;
@@ -48,6 +59,6 @@ export const clearCart = async (prevstate, formData) => {
       headers: { "content-type": "application/json" },
       body: JSON.stringify({ cart }),
     });
-    revalidateTag("navbar")
+    revalidateTag("navbar");
   }
 };
