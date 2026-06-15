a785369dd2995bb31c93717e22d79dc471e8b827
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-15 14:09:26 +0300

    refactor: refactor code

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,18 +1,11 @@
-"use client"
-import React from "react";
-import Sidebar from "./sidbar/sidbar"; // ⚠️ عدل المسار ده لمكان ملف الـ Sidebar بتاعك
-import styles from "./layout.module.css"; // ملف الـ CSS الأساسي للـ Layout
-
+"use client";
+import Sidebar from "@/Components/dashboardSidbar/sidbar";
+import styles from "./layout.module.css";
 export default function DashboardLayout({ children }) {
   return (
     <div className={styles.layoutContainer}>
-      {/* الـ Sidebar هيثبت هنا ومش هيتكرر كوده ولا هيحمل من جديد بين الصفحات */}
       <Sidebar />
-      
-      {/* الـ Main Content اللي بيتغير حسب الصفحة (Dashboard, Running, Customers) */}
-      <main className={styles.mainContent}>
-        {children}
-      </main>
+      <main className={styles.mainContent}>{children}</main>
     </div>
   );
 }
