5ed2ef2fa49c0d351514a9746068f5fe38f931b9
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-15 14:03:12 +0300

    refactor: refactor code from json server to supabase to get users and  viewed detils users (oreders list and total revenue)

--- server/customer_server.jsx
+++ server/customer_server.jsx
@@ -1,4 +1,5 @@
 "use server";
+import { createClientForServer } from "@/utils/supabase";
 import { revalidateTag } from "next/cache";
 
 export default async function HandleAtion(prevend, formData) {
@@ -6,25 +7,38 @@ export default async function HandleAtion(prevend, formData) {
   const message = formData.get("message");
   const block = formData.get("block");
   const inputdialog = formData.get("inputdialog");
-  console.log(block);
 
   try {
     if (search) {
-      const user = await fetch(`http://localhost:1200/users`);
-      if (user.ok) {
-        const userData = await user.json();
-
-        const filteruser = userData.filter((user) => {
-          if (user.name.includes(search)) {
-            return user;
-          } else if (user.email.includes(search)) {
-            return user;
-          }
-        });
-        return filteruser;
-      } else {
+      const supabaseServer = await createClientForServer();
+      const { data: profiles, error: profilesError } = await supabaseServer
+        .from("profiles")
+        .select("*");
+      if (profilesError) {
         return [];
       }
+      console.log(profiles);
+
+      const filteruser = profiles.filter((user) => {
+        user.full_name?.includes(search) 
+          // user.email?.toLowerCase().includes(search.toLowerCase());
+      });
+      
+      if (filteruser.length === 0) {
+        return {
+          users: [],
+          foundState: 401,
+          message: "Not Found User",
+          time: Date.now(),
+        };
+      }
+
+      return {
+        users: filteruser,
+        foundState: 200,
+        message: "",
+        time: Date.now(),
+      };
     } else if (block) {
       if (block && !inputdialog) {
         return { blockState: true, timeStamp: Date.now() };
@@ -55,4 +69,63 @@ export default async function HandleAtion(prevend, formData) {
     console.error(error);
     return [];
   }
+}
+
+{
+  // "use server";
+  // import { createClientForServer } from "@/utils/supabase";
+  // import { revalidateTag } from "next/cache";
+  // export default async function HandleAtion(prevend, formData) {
+  //   const search = formData.get("textvalue");
+  //   const message = formData.get("message");
+  //   const block = formData.get("block");
+  //   const inputdialog = formData.get("inputdialog");
+  //   try {
+  //     if (search) {
+  //       const supabaseServer = await createClientForServer();
+  //       const { data: profiles, error: profilesError } = await supabaseServer
+  //         .from("profiles")
+  //         .select("*");
+  //       if (profilesError) {
+  //         return [];
+  //       }
+  //        profiles.filter((user) => {
+  //         if (user.full_name.includes(search) || user.email.includes(search)) {
+  //           console.log("yes");
+  //           return user;
+  //         } else {
+  //           return { fou: 401, message: "Not Found User" };
+  //         }
+  //       });
+  //     } else if (block) {
+  //       if (block && !inputdialog) {
+  //         return { blockState: true, timeStamp: Date.now() };
+  //       }
+  //       if (inputdialog === "yes") {
+  //         const user = await fetch(`http://localhost:1200/users/${block}`);
+  //         if (user.ok) {
+  //           let userdata = await user.json();
+  //           await fetch(`http://localhost:1200/blocked`, {
+  //             method: "POST",
+  //             headers: { "Content-type": "application/json" },
+  //             body: JSON.stringify(userdata),
+  //           });
+  //           const deleteUser = await fetch(`http://localhost:1200/users`);
+  //           if (deleteUser.ok) {
+  //             await fetch(`http://localhost:1200/users/${block}`, {
+  //               cache: "no-cache",
+  //               method: "DELETE",
+  //             });
+  //             revalidateTag("cuctomerPage");
+  //             revalidateTag("usersBlock");
+  //             return { blockState: false };
+  //           }
+  //         }
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //     return [];
+  //   }
+  // }
 }
