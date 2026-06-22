2182dbc1f74ea4c19d033cbe2c897eb91b06f981
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-06-22 07:40:36 +0300

    refactor: refactor code

--- server/blockList_server.jsx
+++ server/blockList_server.jsx
@@ -1,38 +1,60 @@
 "use server";
 
-import { revalidateTag } from "next/cache";
+import { createClientForServer } from "@/utils/supabase";
+import { revalidatePath } from "next/cache";
 
 export default async function UnblockUsers(prevstate, formData) {
-  const BlockBut = formData.get("BlockBut");
-  const inputdialog = formData.get("inputdialog");
+  const unblock = formData.get("unblock");
+  const user = formData.get("user");
 
-  try {
-    if (BlockBut && !inputdialog) {
-      return { blockState: true, timeStamp: Date.now() };
+  if (unblock) {
+    try {
+      const supabaseServer = await createClientForServer();
+
+      const { data: getuser, error: userError } = await supabaseServer
+        .from("blacklist")
+        .select("*")
+        .eq("id", user);
+
+      if (userError || !getuser || getuser.length === 0) {
+        return {
+          deletingsuccess: false,
+          message: "User not found in blacklist or database error",
+        };
+      }
+
+      const { error: unblockError } = await supabaseServer
+        .from("profiles")
+        .insert(getuser);
+
+      if (unblockError) {
+        console.error("Profiles Insert Error (Unblock):", unblockError);
+        return {
+          deletingsuccess: false,
+          message: "Failed to restore user to profiles",
+        };
+      }
+
+      const { error: deletedError } = await supabaseServer
+        .from("blacklist")
+        .delete()
+        .eq("id", user);
+
+      if (deletedError) {
+        console.error("Error during blacklist deletion:", deletedError);
+        return {
+          deletingsuccess: false,
+          message: "User restored but failed to remove from blacklist table",
+        };
+      }
+
+      revalidatePath("/dashboard/customer");
+      revalidatePath("/dashboard/blockList");
+
+      return { deletingsuccess: true, message: "User unblocked successfully" };
+    } catch (error) {
+      console.error("Critical System Error during Unblock:", error);
+      redirect("/error?type=technical");
     }
-    if (inputdialog === "yes") {
-
-      const fineduser = await fetch(
-        `http://localhost:1200/blocked/${BlockBut}`,
-        {
-          cache: "no-store",
-        },
-      );
-      const user = await fineduser.json();
-      await fetch(`http://localhost:1200/users`, {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(user),
-      });
-
-      await fetch(`http://localhost:1200/blocked/${BlockBut}`, {
-        method: "DELETE",
-      });
-      revalidateTag("usersBlock");
-      revalidateTag("cuctomerPage");
-      return { blockState: false };
-    }
-  } catch (error) {
-    console.error(error);
   }
 }
