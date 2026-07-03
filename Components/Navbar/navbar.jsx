1dacf136b29759a44f47b062d681a5d1eebca03b
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-04 02:45:22 +0300

    refactor: update speed-insights import path

--- Components/Navbar/navbar.jsx
+++ Components/Navbar/navbar.jsx
@@ -11,8 +11,8 @@ import MostoreLogo from "../my_logo/logo";
 
 function NavBar({ userdata }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [isMounted, setIsMounted] = useState(false); 
-  
+  const [isMounted, setIsMounted] = useState(false);
+
   const dispatch = useDispatch();
   const pathname = usePathname();
   const wishlist = useSelector((state) => state.wishlist.items);
@@ -20,7 +20,7 @@ function NavBar({ userdata }) {
 
   useEffect(() => {
     const initialize = () => {
-      setIsMounted(true); 
+      setIsMounted(true);
 
       if (userdata?.wishlist) {
         dispatch(setInitialWishlist(userdata.wishlist));
@@ -43,11 +43,21 @@ function NavBar({ userdata }) {
         </Link>
 
         <div className={`${style.nav_links} ${style.desktop_menu}`}>
-          <Link href="/" className={style.link}>HOME</Link>
-          <Link href="/man" className={style.link}>MEN</Link>
-          <Link href="/woman" className={style.link}>WOMEN</Link>
-          <Link href="/Child" className={style.link}>KIDS</Link>
-          <Link href="/Admin/dashboard" className={style.link}>ADMIN</Link>
+          <Link href="/" className={style.link}>
+            HOME
+          </Link>
+          <Link href="/man" className={style.link}>
+            MEN
+          </Link>
+          <Link href="/woman" className={style.link}>
+            WOMEN
+          </Link>
+          <Link href="/Child" className={style.link}>
+            KIDS
+          </Link>
+          <Link href="/Admin/dashboard" className={style.link}>
+            ADMIN
+          </Link>
         </div>
 
         <div className={style.hamburger} onClick={() => setIsOpen(!isOpen)}>
@@ -55,30 +65,55 @@ function NavBar({ userdata }) {
         </div>
 
         <div className={`${style.mobile_menu} ${isOpen ? style.open : ""}`}>
-          <Link href="/" onClick={() => setIsOpen(false)}>HOME</Link>
-          <Link href="/man" onClick={() => setIsOpen(false)}>MEN</Link>
-          <Link href="/woman" onClick={() => setIsOpen(false)}>WOMEN</Link>
-          <Link href="/Child" onClick={() => setIsOpen(false)}>KIDS</Link>
-          <Link href="/Admin/dashboard" onClick={() => setIsOpen(false)}>ADMIN</Link>
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            HOME
+          </Link>
+          <Link href="/man" onClick={() => setIsOpen(false)}>
+            MEN
+          </Link>
+          <Link href="/woman" onClick={() => setIsOpen(false)}>
+            WOMEN
+          </Link>
+          <Link href="/Child" onClick={() => setIsOpen(false)}>
+            KIDS
+          </Link>
+          <Link href="/Admin/dashboard" onClick={() => setIsOpen(false)}>
+            ADMIN
+          </Link>
+          <div className={style.search_container}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className={style.searchInput}
+            />
+            <i
+              className={`fa-solid fa-magnifying-glass ${style.search_icon}`}
+            ></i>
+          </div>
         </div>
 
         <div className={style.right_section}>
           <div className={style.search_container}>
-            <input type="text" placeholder="Search..." className={style.searchInput} />
-            <i className={`fa-solid fa-magnifying-glass ${style.search_icon}`}></i>
+            <input
+              type="text"
+              placeholder="Search..."
+              className={style.searchInput}
+            />
+            <i
+              className={`fa-solid fa-magnifying-glass ${style.search_icon}`}
+            ></i>
           </div>
 
           <ThemeToggle />
 
           <div className={style.icon_group}>
-
             <Link href="/Wishlist" className="relative">
               <i className="fa-regular fa-heart"></i>
               {isMounted && wishlist?.length > 0 && (
                 <span className={style.badge}>{wishlist.length}</span>
               )}
             </Link>
-          
+
             <Link href="/CardPage" className="relative">
               <i className="fa-solid fa-bag-shopping "></i>
               {isMounted && card?.length > 0 && (
@@ -86,7 +121,7 @@ function NavBar({ userdata }) {
               )}
             </Link>
 
-            <Link href="/Profile" >
+            <Link href="/Profile">
               <i className="fa-regular fa-user"></i>
             </Link>
           </div>
