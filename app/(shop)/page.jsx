869d38e13656f23cbdfe0468ddbb9b1dd783c90e
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-06 03:01:44 +0300

    refactor: refactor code

--- app/(shop)/page.jsx
+++ app/(shop)/page.jsx
@@ -6,13 +6,11 @@ import Footer from "@/Components/footer/Footre";
 import SportComponete from "@/Components/your_sport_start_here_componente/you_sport_componete";
 import DiscoundComponent from "@/Components/discound_componente/discounds";
 import HotCOMPONANTE from "@/Components/what_is_hot_componante/what_is_hot";
-import NavAction from "@/Components/Navbar/NavAction";
 import Feedback from "@/Components/feedback_component/feedback";
-// import "aos/dist/aos.css";
+
 export default async function Home() {
   return (
     <>
-      <NavAction />
       <Feedback />
       <Hreo />
       <Sport />
