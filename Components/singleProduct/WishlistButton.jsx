e5f79cd953f94791ac1a3a0d2f6130c77a4b71f5
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-11 18:35:37 +0300

    feat(wishlist): add guest wishlist support via localStorage and clean up state

--- Components/singleProduct/WishlistButton.jsx
+++ Components/singleProduct/WishlistButton.jsx
@@ -1,52 +1,54 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./page.module.css";
-import { useActionState, useEffect } from "react";
+import { useActionState, useEffect, useState } from "react";
 import { toggleWishlistOptimistic } from "@/RTK/wishlistslice";
 import { useDispatch, useSelector } from "react-redux";
 import handleAction from "@/server/hero_server";
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
 import LoaderTag from "@/Components/loaderFecthing/loaderTofetch";
-import { useOpneing } from "@/RTK/storcontext";
 import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+
 
 export default function WishlistButton({ favoriteProduct }) {
-  const Router = useRouter();
+  const [isfevorite, setisfevorite] = useState(false);
   const initialState = { message: "", wishliststate: null };
   const [state, formAction, pending] = useActionState(
     handleAction,
     initialState,
   );
   const dispatch = useDispatch();
-  const { setisfevorite } = useOpneing();
   const handleWishlistSubmit = () => {
     dispatch(toggleWishlistOptimistic(favoriteProduct));
   };
 
-  const wishlistarray = useSelector((state) => state.wishlist.items);
-
-  const isfevorite = wishlistarray.some(
-    (item) => Number(item.id) === favoriteProduct.id,
+  const wishlistarray = useSelector((state) => state.wishlist.items) || [];
+  const isfev = wishlistarray.some(
+    (item) => Number(item?.id) === Number(favoriteProduct?.id),
   );
 
   useEffect(() => {
-    if (state?.state === 401) {
-      toast.error("Login Required", {
-        description: "Please log in to continue. Redirecting...",
-        position: "bottom-right",
-        duration: 3000,
-        onAutoClose: () => {
-          Router.replace("/register");
-        },
-      });
-      return;
+    if (!state) return;
+    if (state?.state === 201) {
+      const productData = state.guestproduct;
+      if (productData) {
+        let localWishlist =
+          JSON.parse(localStorage.getItem("guest_wishlist")) || [];
+        const exists = localWishlist.some((item) => item.id === productData.id);
+
+        if (exists) {
+          localWishlist = localWishlist.filter(
+            (item) => item.id !== productData.id,
+          );
+        } else {
+          localWishlist = [...localWishlist, productData];
+        }
+        localStorage.setItem("guest_wishlist", JSON.stringify(localWishlist));
+      }
     }
 
     if (state?.wishliststate !== undefined && state?.wishliststate !== null) {
-      setisfevorite(state.wishliststate);
-
       if (state.wishliststate) {
         toast.success("Added to Wishlist", {
           position: "bottom-right",
@@ -59,7 +61,11 @@ export default function WishlistButton({ favoriteProduct }) {
         });
       }
     }
-  }, [state?.wishliststate, state?.state, setisfevorite, Router]);
+  }, [state]);
+
+  useEffect(() => {
+    setisfevorite(isfev);
+  }, [isfev]);
 
   return (
     <form action={formAction}>
