7d7e688fd6834a524f006203a20f60b80d7b2388
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-12 00:45:32 +0300

    feat: Use the zod library to perform data validation and data protection

--- app/(shop)/Admin/products/[product]/serverFile.jsx
+++ app/(shop)/Admin/products/[product]/serverFile.jsx
@@ -2,14 +2,56 @@
 
 import { createClientForServer } from "@/utils/supabase";
 import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const productUpdateSchema = z.object({
+  id: z.any(),
+  type: z.string().optional().nullable(),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().optional().nullable(),
+  price: z.coerce.number({ invalid_type_error: "Price must be a number" }),
+  oldprice: z.preprocess(
+    (val) => (val === "" || val === undefined ? null : val),
+    z.coerce.number().nullable().optional(),
+  ),
+  stock: z.preprocess(
+    (val) => (val === "" || val === undefined ? null : val),
+    z.coerce.number().nullable().optional(),
+  ),
+  Inventory: z
+    .preprocess(
+      (val) => (val === "" || val === undefined ? null : val),
+      z.coerce.number().nullable().optional(),
+    )
+    .optional(),
+
+  image: z.string().optional().nullable(),
+  image_Hover: z.string().optional().nullable(),
+  image3: z.string().optional().nullable(),
+  image4: z.string().optional().nullable(),
+  video: z.string().optional().nullable(),
+});
 
 export default async function EditProduct(prevstate, formdata) {
   const product = JSON.parse(formdata.get("product"));
 
-  try {
+  const validation = productUpdateSchema.safeParse(product);
 
+  if (!validation.success) {
+    console.error("Zod Validation Error:", validation.error.format());
+    return {
+      success: false,
+      message:
+        "Product data is invalid, please include it from the digital source.",
+    };
+  }
+
+  const cleanedData = validation.data;
+
+  try {
     const supabaseServer = await createClientForServer();
-    const { id, ...dataToUpdata } = product;
+    const { id, ...dataToUpdata } = cleanedData;
+
     const { error: productError } = await supabaseServer
       .from("products")
       .update(dataToUpdata)
@@ -22,7 +64,6 @@ export default async function EditProduct(prevstate, formdata) {
         message: "Failed to update product. Please try again later.",
       };
     }
-
 
     revalidatePath("/");
     return { success: true, message: "Product updated successfully!" };
