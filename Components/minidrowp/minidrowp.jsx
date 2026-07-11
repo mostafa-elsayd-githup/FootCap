cc462f210e4e59f30a58f01f26a9f2601b546088
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-11 21:24:33 +0300

    refactor: refactor code and use redux  to manage state

--- Components/minidrowp/minidrowp.jsx
+++ Components/minidrowp/minidrowp.jsx
@@ -3,167 +3,15 @@ import styles from "./minidrowp.module.css";
 import { Card } from "react-bootstrap";
 import { useOpneing } from "@/RTK/storcontext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faRightLong } from "@fortawesome/free-solid-svg-icons";
-import handelAction from "@/Components/minidrowp/miniaction";
-import { useEffect, useState } from "react";
-import { useActionState } from "react";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import Link from "next/link";
-import Swal from "sweetalert2";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCartOptimistic, removeFromCartOptimistic } from "@/RTK/cardslice";
-import { toggleWishlistOptimistic } from "@/RTK/wishlistslice";
-import Loader from "@/Components//loaderFecthing/loader";
+import Buttons from "./minidropButton";
 export default function MiniDrowp() {
-  const Router = useRouter();
-  const dispatch = useDispatch();
-  const { isOpen, setIsOpen, selectedProduct, setisfevorite } = useOpneing();  
-  const initialState = { massage: "", wishliststate: null };
-  const [state, formAction, pending] = useActionState(
-    handelAction,
-    initialState,
-  );
-  console.log(selectedProduct);
-  
-  const [actionTypeState, setActionTypeState] = useState("");
-  const [selectedSize, setselectedSize] = useState("");
-  const [AddToCart, setAddToCart] = useState(false);
-  const [lastTimestamp, setLastTimestamp] = useState(null);
-  const wishlistItems = useSelector((state) => state.wishlist.items);
-  const isfevorite = wishlistItems.some(
-    (item) => Number(item.id) === Number(selectedProduct?.id),
-  );
-
-  const handleaddedtocard = async () => {
-    setActionTypeState("card");
-    setLastTimestamp(Date.now());
-    const productWithCartId = {
-      ...selectedProduct,
-      id: `${selectedProduct.id}-${selectedSize}`,
-    };
-    dispatch(addToCartOptimistic(productWithCartId));
-  };
-  const handlewishlist = async () => {
-    setActionTypeState("wishlist");
-    setLastTimestamp(Date.now());
-    dispatch(toggleWishlistOptimistic(selectedProduct));
-  };
+  const { isOpen, setIsOpen, selectedProduct, setAddToCart, setselectedSize, selectedSize } = useOpneing();
   const handleViewProductButton = async () => {
-    if (state?.tokenstate === 401) {
-      Swal.fire({
-        title: "Login Required",
-        text: "Please log in to continue. Redirecting...",
-        icon: "error",
-        timer: 3000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-        willClose: () => {
-          // <=callback function
-          Router.replace("/register");
-        },
-      });
-    } else {
-      redirect(`/product/${selectedProduct.id}`);
-    }
+    redirect(`/product/${selectedProduct.id}`);
   };
-
-  useEffect(() => {
-    setActionTypeState("");
-  }, [selectedProduct]);
-
-  useEffect(() => {
-    if (state?.tokenstate === 401) {
-      Swal.fire({
-        title: "Login Required",
-        text: "Please log in to continue. Redirecting...",
-        icon: "error",
-        timer: 1000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-        willClose: () => {
-          Router.replace("/register");
-        },
-      });
-      return;
-    }
-
-    if (
-      actionTypeState === "wishlist" &&
-      state?.wishliststate !== undefined &&
-      state?.wishliststate !== null &&
-      state?.timeStamp > lastTimestamp
-    ) {
-      setActionTypeState("");
-
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom-left",
-        showConfirmButton: false,
-        timer: 1000,
-      });
-      Toast.fire({
-        icon: "success",
-        title: state.wishliststate
-          ? "Added to Wishlist "
-          : "Removed from Wishlist",
-      });
-      setTimeout(() => {
-        setIsOpen(false);
-        setselectedSize("");
-        setAddToCart(false);
-      }, state.wishliststate);
-    }
-    if (
-      actionTypeState === "card" &&
-      state?.cardState !== undefined &&
-      state?.cardState !== null &&
-      state?.timeStamp > lastTimestamp
-    ) {
-      setActionTypeState("");
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom-left",
-        showConfirmButton: false,
-        timerProgressBar: true,
-        timer: 2000,
-      });
-      Toast.fire({
-        icon: "success",
-        title: state.cardState ? "quantity +1" : "Added to Cart",
-      });
-
-      setTimeout(() => {
-        setIsOpen(false);
-        setselectedSize("");
-        setAddToCart(false);
-      }, state.wishliststate);
-    }
-
-    if (state?.status === 500) {
-      if (actionTypeState === "wishlist") {
-        dispatch(rollbackWishlist(selectedProduct));
-      } else if (actionTypeState === "card") {
-        dispatch(removeFromCartOptimistic(selectedProduct.id));
-      }
-
-      Swal.fire({
-        title: "Error",
-        text: state.message || "Something went wrong",
-        icon: "error",
-      });
-    }
-  }, [
-    state,
-    actionTypeState,
-    lastTimestamp,
-    Router,
-    setisfevorite,
-    setIsOpen,
-    selectedProduct,
-    dispatch,
-  ]);
   const oldprice = Intl.NumberFormat("en", {
     notation: "standard",
     style: "currency",
@@ -178,15 +26,16 @@ export default function MiniDrowp() {
   }).format(parseInt(selectedProduct?.price));
   return (
     <>
-      {pending && <Loader/>}
       {selectedProduct && (
         <div
           className={`${styles.overlay} ${isOpen ? styles.activeOverlay : ""}`}
-           onClick={() => setIsOpen(false)}
+          onClick={() => setIsOpen(false)}
         >
-          {console.log(selectedProduct)
-          }
-          <div key={selectedProduct.id} className={styles.container} onClick={(e) => e.stopPropagation()}>
+          <div
+            key={selectedProduct.id}
+            className={styles.container}
+            onClick={(e) => e.stopPropagation()}
+          >
             <div className={styles.imageGallery}>
               <div className={styles.imageContainer}>
                 <Card.Img
@@ -270,11 +119,6 @@ export default function MiniDrowp() {
                       {size}
                     </button>
                   ))}
-                  {state ? (
-                    <span className="text-(--color-sale) w-36 content-center">
-                      {state?.message}
-                    </span>
-                  ) : null}
                 </div>
               </div>
 
@@ -289,76 +133,10 @@ export default function MiniDrowp() {
                     <FontAwesomeIcon icon={faRightLong} />
                   </span>
                 </Link>
-                <form className={styles.icons} action={formAction}>
-                  <>
-                    {/*data for ActionFile*/}
-                    <input
-                      type="hidden"
-                      name="id"
-                      value={selectedProduct.id || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="image"
-                      value={selectedProduct.image || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="name"
-                      value={selectedProduct.name || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="dis"
-                      value={selectedProduct.dis || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="price"
-                      value={selectedProduct.price || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="size"
-                      value={selectedSize || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="category"
-                      value={selectedProduct.category || ""}
-                    />
-                    <input
-                      type="hidden"
-                      name="actiontype"
-                      value={actionTypeState || ""}
-                    />
-                  </>
-                  <button
-                    className={`${styles.addToCartBtn} ${AddToCart === false ? styles.activeBut : ""}`}
-                    type="submit"
-                    onClick={handleaddedtocard}
-                  >
-                    ADD TO BAG
-                    <span className={styles.arrowIcon}>
-                      <FontAwesomeIcon icon={faRightLong} />
-                    </span>
-                  </button>
-                  <button
-                    className={styles.wishlistBtn}
-                    type="submit"
-                    disabled={pending}
-                    onClick={handlewishlist}
-                    style={{ opacity: pending ? 0.5 : 1 }}
-                  >
-                    <FontAwesomeIcon
-                      className={styles.icon}
-                      icon={isfevorite ? fasHeart : farHeart}
-                    />
-                  </button>
-                </form>
+
+                <Buttons />
               </div>
             </div>
-
           </div>
         </div>
       )}
