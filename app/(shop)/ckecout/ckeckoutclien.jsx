d170ba3f97e8270a2c843e396ff409506f37bdb2
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-26 15:30:48 +0300

    feat(checkout): integrate Stripe integration, forms, and processing UI

--- app/(shop)/ckecout/ckeckoutclien.jsx
+++ app/(shop)/ckecout/ckeckoutclien.jsx
@@ -1,202 +1,27 @@
 "use client";
-import { useState, useEffect, useActionState } from "react";
 import styles from "./ckeckout.module.css";
-import Swal from "sweetalert2";
-import { useRouter } from "next/navigation";
 import { Card } from "react-bootstrap";
-import handleOrder from "@/server/ckeckoutServer";
-import Loader from "@/Components/loaderFecthing/loader";
+import Form from "./form";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import ProcessingOverlay from "./Processing";
 export default function CheckoutPage({ cartItems }) {
-  const router = useRouter();
-  const [FormDataClient, setFormDataClient] = useState({
-    fullName: "",
-    val: "",
-    address: "",
-    card: "",
-    phone: "",
-    city: "",
-    totalPrice: "",
-  });
-  const initialState = { message: "", wishliststate: null };
-  const [state, formAction, pending] = useActionState(
-    handleOrder,
-    initialState,
-  );
   const subtotal =
-    cartItems?.reduce(
-      (acc, item) => acc + item?.price * item?.quantity,
-      0,
-    ) || 0;
-
+    cartItems?.reduce((acc, item) => acc + item?.price * item?.quantity, 0) ||
+    0;
   const shipping = 50;
-
   const total = subtotal + shipping;
-  const egyptGovernorates = [
-    "Cairo",
-    "Giza",
-    "Alexandria",
-    "Dakahlia",
-    "Red Sea",
-    "Beheira",
-    "Fayoum",
-    "Gharbia",
-    "Ismailia",
-    "Menofia",
-    "Minya",
-    "Qalyubia",
-    "New Valley",
-    "Suez",
-    "Aswan",
-    "Assiut",
-    "Beni Suef",
-    "Port Said",
-    "Damietta",
-    "Sharkia",
-    "South Sinai",
-    "Kafr El Sheikh",
-    "Matrouh",
-    "Luxor",
-    "Qena",
-    "North Sinai",
-    "Sohag",
-  ];
-  useEffect(() => {
-    if (state?.inputState === 100) {
-      Swal.fire("Error", `${state?.message}`, "error");
-    } else if (
-      state?.inputState === 101 ||
-      state?.inputState === 102 ||
-      state?.inputState === 103
-    ) {
-      Swal.fire("Error", `${state?.message}`, "error");
-    } else if (state?.success) {
-      setTimeout(() => {
-        Swal.fire({
-          title: "Order Placed!",
-          text: "Your order has been received successfully.",
-          icon: "success",
-          confirmButtonColor: "#000",
-        }).then(() => {
-          router.push("/");
-        });
-      }, state.success);
-    }
-  }, [
-    state.timeStamp,
-    state?.inputState,
-    state?.success,
-    state?.message,
-    router,
-    state,
-  ]);
-
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  );
   return (
     <div className={styles.checkoutContainer}>
-      {pending && <Loader />}
-
+      <ProcessingOverlay/>
       <div className={styles.shippingSection}>
         <h2 className={styles.title}>Shipping Information</h2>
-        <form action={formAction} className={styles.formGrid}>
-          <div className={styles.formGroup}>
-            <label>Full Name</label>
-            <input
-              type="text"
-              className={styles.inputField}
-              placeholder="Enter your full name"
-              onChange={(e) =>
-                setFormDataClient({
-                  ...FormDataClient,
-                  fullName: e.target.value,
-                })
-              }
-            />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Phone Number</label>
-            <input
-              type="tel"
-              className={styles.inputField}
-              placeholder="01xxxxxxxxx"
-              maxLength={11}
-              onChange={(e) => {
-                let val = e.target.value.replace(/\D/g, "");
-                setFormDataClient({ ...FormDataClient, phone: val });
-              }}
-            />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Detailed Address</label>
-            <input
-              type="text"
-              className={styles.inputField}
-              placeholder="Building, Street, Area"
-              onChange={(e) =>
-                setFormDataClient({
-                  ...FormDataClient,
-                  address: e.target.value,
-                })
-              }
-            />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Credit Card</label>
-            <input
-              type="text"
-              className={styles.inputField}
-              placeholder="0000 0000 0000 0000"
-              maxLength={16}
-              onChange={(e) => {
-                let val = e.target.value.replace(/\D/g, "");
-                setFormDataClient({
-                  ...FormDataClient,
-                  card: val,
-                });
-              }}
-            />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>City {`( ${egyptGovernorates.length} )`}</label>
-            <input
-              list="egypt-cities"
-              className={styles.inputField}
-              placeholder="Select city...."
-              onChange={(e) =>
-                setFormDataClient({ ...FormDataClient, city: e.target.value })
-              }
-            />
-            <datalist id="egypt-cities">
-              {egyptGovernorates.map((item) => (
-                <option key={item} value={item} />
-              ))}
-            </datalist>
-          </div>
-
-          <button type="submit" className={styles.orderBtn}>
-            CONFIRM ORDER
-          </button>
-          <input
-            type="hidden"
-            name="fullName"
-            value={FormDataClient.fullName}
-          />
-          <input type="hidden" name="address" value={FormDataClient.address} />
-          <input type="hidden" name="phone" value={FormDataClient.phone} />
-          <input type="hidden" name="city" value={FormDataClient.city} />
-          <input type="hidden" name="card" value={FormDataClient.card} />
-          <input type="hidden" name="totalprice" value={total} />
-          {cartItems.map((item) => (
-            <input
-              key={item.id}
-              type="hidden"
-              name="allProducts"
-              value={JSON.stringify(item)}
-            />
-          ))}
-        </form>
+        <Elements stripe={stripePromise}>
+          <Form cartItems={cartItems} />
+        </Elements>
       </div>
 
       <div className={styles.summarySection}>
@@ -220,9 +45,7 @@ export default function CheckoutPage({ cartItems }) {
                   {item.quantity > 1 && (
                     <span className={styles.itemTotalPrice}>
                       Total: EGP{" "}
-                      {(
-                        Number(item.price) * item.quantity
-                      ).toLocaleString()}
+                      {(Number(item.price) * item.quantity).toLocaleString()}
                     </span>
                   )}
                 </div>
---
+++ app/(shop)/ckecout/form.jsx
@@ -0,0 +1,248 @@
+"use client";
+import styles from "./ckeckout.module.css";
+import { useRouter } from "next/navigation";
+import { useActionState, useState, useEffect, startTransition } from "react";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import handleOrder from "@/server/ckeckoutServer";
+import ErrorMessage from "./errorMessge";
+import { toast } from "sonner";
+import ProcessingOverlay from "./Processing";
+export default function Form({ cartItems }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [stripeToken, setStripeToken] = useState();
+
+  const router = useRouter();
+
+  const initialState = {
+    message: "",
+    wishliststate: null,
+    success: null,
+    inputState: null,
+  };
+  const [state, formAction, pending] = useActionState(
+    handleOrder,
+    initialState,
+  );
+  const [paymentMethod, setPaymentMethod] = useState("cash");
+
+  const [FormDataClient, setFormDataClient] = useState({
+    fullName: "",
+    address: "",
+    phone: "",
+    city: "",
+  });
+
+  const egyptGovernorates = [
+    "Cairo",
+    "Giza",
+    "Alexandria",
+    "Dakahlia",
+    "Red Sea",
+  ];
+  useEffect(() => {
+    if (state?.inputState) {
+      toast.error(state?.message);
+    } else if (state?.success) {
+      toast.success("Your order has been received successfully");
+
+      startTransition(() => {
+        setFormDataClient({
+          fullName: "",
+          address: "",
+          phone: "",
+          city: "",
+        });
+      });
+      setTimeout(() => {
+        router.push("/");
+      }, 3000);
+    }
+  }, [state, router]);
+  const subtotal =
+    cartItems?.reduce((acc, item) => acc + item?.price * item?.quantity, 0) ||
+    0;
+  const shipping = 50;
+  const total = subtotal + shipping;
+  const handleSubmit = async (e) => {
+    if (paymentMethod === "cash") {
+      return;
+    }
+
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const formElement = e.currentTarget;
+    const cardElement = elements.getElement(CardElement);
+
+    const { token, error } = await stripe.createToken(cardElement);
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    const formData = new FormData(formElement);
+    formData.set("stripeToken", token.id);
+
+    startTransition(() => {
+      formAction(formData);
+    });
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      action={formAction}
+      className={styles.formGrid}
+    >
+      <ProcessingOverlay isopen={pending}/>
+      <div className={styles.formGroup}>
+        <label>Full Name</label>
+        <input
+          type="text"
+          value={FormDataClient.fullName}
+          className={styles.inputField}
+          placeholder="Enter your full name"
+          onChange={(e) =>
+            setFormDataClient({
+              ...FormDataClient,
+              fullName: e.target.value,
+            })
+          }
+        />
+        <ErrorMessage message={state?.errors?.fullName} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label>Phone Number</label>
+        <input
+          type="tel"
+          value={FormDataClient.phone}
+          className={styles.inputField}
+          placeholder="01xxxxxxxxx"
+          maxLength={11}
+          onChange={(e) =>
+            setFormDataClient({
+              ...FormDataClient,
+              phone: e.target.value.replace(/\D/g, ""),
+            })
+          }
+        />
+        <ErrorMessage message={state?.errors?.phone} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label>Detailed Address</label>
+        <input
+          type="text"
+          value={FormDataClient.address}
+          className={styles.inputField}
+          placeholder="Building, Street, Area"
+          onChange={(e) =>
+            setFormDataClient({
+              ...FormDataClient,
+              address: e.target.value,
+            })
+          }
+        />
+        <ErrorMessage message={state?.errors?.address} />
+      </div>
+
+      <div className={styles.formGroup}>
+        <label>City</label>
+        <input
+          list="egypt-cities"
+          value={FormDataClient.city}
+          className={styles.inputField}
+          placeholder="Select city...."
+          onChange={(e) =>
+            setFormDataClient({ ...FormDataClient, city: e.target.value })
+          }
+        />
+        <datalist id="egypt-cities">
+          {egyptGovernorates.map((item) => (
+            <option key={item} value={item} />
+          ))}
+        </datalist>
+        <ErrorMessage message={state?.errors?.city} />
+      </div>
+
+      <div className={styles.paymentMethodSection}>
+        <h3>Select Payment Method</h3>
+        <div className={styles.radioGroup}>
+          <label className={paymentMethod === "cash" ? styles.activeRadio : ""}>
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="cash"
+              checked={paymentMethod === "cash"}
+              onChange={() => setPaymentMethod("cash")}
+            />{" "}
+            💵 Cash on Delivery
+          </label>
+
+          <label className={paymentMethod === "card" ? styles.activeRadio : ""}>
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="card"
+              checked={paymentMethod === "card"}
+              onChange={() => setPaymentMethod("card")}
+            />
+            {" "}
+            💳 Credit / Debit Card
+          </label>
+        </div>
+      </div>
+
+      {paymentMethod === "card" && (
+        <div className={styles.cardDetailsGrid}>
+          <div className={styles.formGroup}>
+            <label>Card Details</label>
+            <div className={styles.stripeWrapper}>
+              <CardElement
+                options={{
+                  style: {
+                    base: {
+                      fontSize: "16px",
+                      color: "#32325d",
+                      fontFamily: "inherit",
+                      "::placeholder": {
+                        color: "#aab7c4",
+                      },
+                    },
+                    invalid: {
+                      color: "#fa755a",
+                      iconColor: "#fa755a",
+                    },
+                  },
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+      <button type="submit" className={styles.orderBtn}>
+        CONFIRM ORDER
+      </button>
+
+      <input type="hidden" name="fullName" value={FormDataClient.fullName} />
+      <input type="hidden" name="address" value={FormDataClient.address} />
+      <input type="hidden" name="phone" value={FormDataClient.phone} />
+      <input type="hidden" name="city" value={FormDataClient.city} />
+      <input type="hidden" name="paymentMethod" value={paymentMethod} />
+
+      <input type="hidden" name="stripeToken" value={stripeToken || ""} />
+      <input type="hidden" name="totalprice" value={total} />
+
+      <input
+        type="hidden"
+        name="allProducts"
+        value={JSON.stringify(cartItems || [])}
+      />
+    </form>
+  );
+}
