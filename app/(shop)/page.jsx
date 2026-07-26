80343f3f30c61d88e0a98f16fbfcdb1bfb2e48b4
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-07-26 15:04:27 +0300

    refactor: delete unseless component

--- app/(shop)/page.jsx
+++ app/(shop)/page.jsx
@@ -1,7 +1,6 @@
 import Hreo from "@/Components/Hero_com/hero";
 import Sport from "@/Components/sport-Componente/Sport";
 import MenCollection from "@/Components/Collection_com/Collection";
-import Footer from "@/Components/footer/Footre";
 import SportComponete from "@/Components/your_sport_start_here_componente/you_sport_componete";
 import DiscoundComponent from "@/Components/discound_componente/discounds";
 import HotCOMPONANTE from "@/Components/what_is_hot_componante/what_is_hot";
@@ -17,7 +16,6 @@ export default async function Home() {
       <SportComponete />
       <HotCOMPONANTE />
       <DiscoundComponent />
-      <Footer />
     </>
   );
 }
