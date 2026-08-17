212fb35356b87a21fb1196feb2f9126a142bdcf8
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-08-17 20:27:22 +0300

    config: add allowed dev origins for local network testing

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'assets.adidas.com',
+        protocol: "https",
+        hostname: "assets.adidas.com",
       },
     ],
   },
-  allowedDevOrigins: ['192.168.1.2'], 
+  allowedDevOrigins: ["192.168.1.2", "192.168.1.10", "192.168.1.20"],
 };
 
 export default nextConfig;
