81257958e412a847397762becfb1518010ac02eb
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-08-28 15:51:35 +0300

    refactor: delete body and html tags to handle file structure

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,20 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 export default function AuthLayout({ children }) {
-  return (
-    // <html lang="en" suppressHydrationWarning>
-       //<body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <div className="auth-container">{children}</div>
-      //</body>
-    //</html>
-  );
+  return <div className="auth-container">{children}</div>;
 }
