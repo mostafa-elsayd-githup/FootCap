d044e01288515f22bbeedab413d859716317b016
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-08-29 14:17:29 +0300

    refactor: delete woman section component

--- app/(shop)/woman/page.jsx
+++ app/(shop)/woman/page.jsx
@@ -1,13 +1,12 @@
+"use client"
 import Section1 from "./section1/section1";
 import Section2 from "./section2/section2";
-import Woman_section from "./woman_section/woman_section";
 import Discound_section from "./discound_product_section/discound_section";
 function Woman() {
   return (
     <>
         <Section1 />
         <Section2 />
-        <Woman_section />
         <Discound_section />
     </>
   );
