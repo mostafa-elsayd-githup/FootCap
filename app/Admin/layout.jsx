3b9faacacbde5b4cb4b9fc84ffc0789a836047cf
Mostafa Elsyd <mostafaalsyd809@gmail.com>
2026-08-29 14:15:35 +0300

    fix: add the navigation bar to the Control Panel layout file

--- app/Admin/layout.jsx
+++ app/Admin/layout.jsx
@@ -1,9 +1,10 @@
-"use client";
 import Sidebar from "@/Components/dashboardSidbar/sidbar";
 import styles from "./layout.module.css";
+import NavAction from "@/Components/Navbar/NavAction";
 export default function DashboardLayout({ children }) {
   return (
     <div className={styles.layoutContainer}>
+      <NavAction />
       <Sidebar />
       <main className={styles.mainContent}>{children}</main>
     </div>
